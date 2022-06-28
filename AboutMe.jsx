a0cb1a411cec19437f8e852db2a7131e9c6d4ba8
Andrew Petersen <fespetersen1990@gmail.com>
2022-06-28 11:13:38 -0600

    Added photos and some css affects

---
+++ AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <div>AboutMe</div>
+  )
+}
+
+
+
+export default AboutMe
---
+++ ReactApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AboutMe from './AboutMe';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AboutMe />
+  </React.StrictMode>,
+  document.getElementById('about-me-title')
+);
